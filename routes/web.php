664d011139f7f390d29dacee94e9d1beff5a6598
DwiFirdaus08 <kirey.zaindeltaperguson@gmail.com>
2024-01-11 12:03:23 +0700

    laravel3

--- routes/web.php
+++ routes/web.php
@@ -21,8 +21,22 @@ Route::get('about',[\App\http\Controllers\Frontend\AboutController::class,'index
 Route::get('/contact', [\App\http\Controllers\Frontend\ContactController::class,'index']);
 Route::post('/contact/store', [\App\http\Controllers\Frontend\ContactController::class,'store'])->name('contact.store');
 
-
+Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('Home');
 
+Route::group(['middleware' => ['auth','is_admin'],'prefix' => 'admin','as' => 'admin.'],function () {
+    Route::get('users', [\App\Http\Controllers\UserController::class, 'index'])->name('users.index');
+
+    Route::get('profile', [\App\Http\Controllers\ProfileController::class, 'show'])->name('profile.show');
+    Route::put('profile', [\App\Http\Controllers\ProfileController::class, 'update'])->name('profile.update');
+    Route::resource('cars', \App\Http\Controllers\Admin\CarController::class);
+    Route::resource('types', \App\Http\Controllers\Admin\TypeController::class);
+    Route::resource('testimonials', \App\Http\Controllers\Admin\TestimonialController::class);
+    Route::resource('teams', \App\Http\Controllers\Admin\TeamController::class);
+    Route::resource('settings', \App\Http\Controllers\Admin\SettingController::class)->only(['index','store','update']);
+    Route::resource('contacts', \App\Http\Controllers\Admin\ContactController::class)->only(['index','destroy']);
+    Route::resource('bookings', \App\Http\Controllers\Admin\BookingController::class)->only(['index','destroy']);
+    Route::resource('blogs', \App\Http\Controllers\Admin\BlogController::class);
+});
 
